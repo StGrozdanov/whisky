07f5969a0c2a372ed8cca5fcdfc29f3c8b93a821
STOYAN GROZDANOV <stoyan.grozdanov@ic-consult.com>
2026-09-13 22:20:27 +0300

    upload playwright report

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 1 : 0,
-  reporter: process.env.CI ? "github" : "list",
+  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "list",
   use: {
     baseURL,
     trace: "on-first-retry",
